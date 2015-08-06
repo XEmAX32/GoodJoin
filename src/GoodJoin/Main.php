af157657503647091523feb93fa4caa186ed08cb
XEmAX32 <XEmAX32@gmail.com>
2015-08-06 22:59:02 +0200

    Update Main.php

--- src/GoodJoin/Main.php
+++ src/GoodJoin/Main.php
@@ -22,7 +22,7 @@ $this->getLogger()->info(TextFormat::BLUE . " [GoodJoin disabled]");
 public function OnPlayerJoin(PlayerJoinEvent $event){
   $players foreach($this->getServer()->getLevelByName("world")->getPlayers());
   $player = $event->getPlayer()->getName();
-$players->sendMessage(TextFormat::GREEN . "+ " .$this->getPlayer()->getName() );
+$players->sendMessage(TextFormat::GREEN . "+  .$this->getPlayer()->getName().");
 $player = $event->getPlayer();
 $message = $this->getConfig()->get("Message");
 $player->sendMessage("$message");
