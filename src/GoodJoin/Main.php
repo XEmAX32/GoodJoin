00f9b49ba9328100ac6169e86a9e139cfce8e937
XEmAX32 <XEmAX32@gmail.com>
2015-08-06 17:43:49 +0200

    Create Main.php

---
+++ src/GoodJoin/Main.php
@@ -0,0 +1,26 @@
+<?php
+namespace GoodJoin;
+  
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerSpawnEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class Main extends PluginBase{
+
+public function OnPlayerSpawn(PlayerSpawnEvent $event){
+  $players = $this->getServer()->getLevelByName("world")->getPlayers();
+  $player = $event->getPlayer()->getName();
+$players->SendMessage(TextFormat::GREEN "[+]" ".$this->getPlayer()->getName().");
+}
+public function OnPlayerQuit(PlayerQuitEvent $event){
+  $players = $this->getServer()->getLevelByName("world")->getPlayers();
+  $player = $event->getPlayer()->getName();
+$players->SendMessage(TextFormat::RED "[-]" ".$this->getPlayer()->getName().");
+}
+public function OnPlayerJoin(PlayerJoinEvent $event){
+  $player = $event->getPlayer();
+  $message = $this->getConfig()->get("Message");
+  $player->sendMessage("$message");
+}
+}
