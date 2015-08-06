203580b706971ea47d7566c64aeb95ff5a90eca6
XEmAX32 <XEmAX32@gmail.com>
2015-08-06 17:59:35 +0200

    Update Main.php

--- src/GoodJoin/Main.php
+++ src/GoodJoin/Main.php
@@ -11,12 +11,12 @@ class Main extends PluginBase{
 public function OnPlayerSpawn(PlayerSpawnEvent $event){
   $players = $this->getServer()->getLevelByName("world")->getPlayers();
   $player = $event->getPlayer()->getName();
-$players->SendMessage(TextFormat::GREEN "+" ".$this->getPlayer()->getName().");
+$players->SendMessage(TextFormat::GREEN " + " ".$this->getPlayer()->getName().");
 }
 public function OnPlayerQuit(PlayerQuitEvent $event){
   $players = $this->getServer()->getLevelByName("world")->getPlayers();
   $player = $event->getPlayer()->getName();
-$players->SendMessage(TextFormat::RED "-" ".$this->getPlayer()->getName().");
+$players->SendMessage(TextFormat::RED " - " ".$this->getPlayer()->getName().");
 }
 public function OnPlayerJoin(PlayerJoinEvent $event){
   $player = $event->getPlayer();
