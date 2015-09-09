b135c0bc0eb264350f47b193e0109eaf231c0a14
XEmAX32 <XEmAX32@gmail.com>
2015-09-09 14:25:12 +0200

    Update Main.php

--- src/GoodJoin/Main.php
+++ src/GoodJoin/Main.php
@@ -21,27 +21,66 @@ $this->getLogger()->info(TextFormat::BLUE . " [GoodJoin disabled]");
 }
 
 public function OnJoin(PlayerJoinEvent $event){
-foreach($this->getServer()->getPlayers() as $players){
-$player = $event->getPlayer();
-$players->sendMessage(TextFormat::GREEN . "[+] " $this->getPlayer()->getName()." ");
-$player = $event->getPlayer();
-$message = $this->getConfig()->get("JoinMessage");
-$player->sendMessage("$joinmessage");
+$name = $e->getPlayer()->getName();
+$player = $e->getPlayer();
+$Effect = $this->getConfig()->get("Effect"));
+$Sound = $this->getConfig()->get("Sound"));
+$Particles = $this->getConfig()->get("Particles"));
+$Command = $this->getConfig()->get("Command");
+$OpJoinMessage = $this->getConfig()->get("OpJoinMessage"));
+$JoinMessage = $this->getConfig()->get("JoinMessage"));
+foreach($this->getServer()->getOnlinePlayers() as $ps){
+  if($player->isOp()){
+    $ps->sendMessage($OpJoinMessage." ");
+      }
+  if($this->getConfig()->get("JoinEffect") == "true"){
+    $
+      }
+  if($this->getConfig()->get("JoinParticles") == "true"){
+      }
+  if($this->getConfig()->get("JoinCommand") == "true"){
+      }
+  if($this->getConfig()->get("JoinSound") == "true"){
+      }
+  if($this->getConfig()->get("MessageAtJoin")){
+  $player->sendMessage($JoinMessage." ");
+      }
+  $player->sendMessage(TextFormat::GREEN . "[+] ".$name);
+        }
+  }
+
+    
+public function OnQuit(PlayerQuitEvent $e){
+$name = $e->getPlayer()->getName();
+$player->sendMessage(TextFormat::RED . "[-] ".$name);
 }
 }
 
-public function OnQuit(PlayerQuitEvent $event){
-  foreach($this->getServer()->getPlayers() sa $ps){
-  $player = $event->getPlayer()->getName();
-  $ps->SendMessage(TextFormat::RED . "[-]" $this->getPlayer()->getName().");
+public function WorldJoinMessage( $e){
+$world = $e->getPlayer()->getLevel()->getName();
+$name = $e->getPlayer()->getName();
+if($this->getConfig()->get("WorldJoinAndQuit") == "true"){
+foreach($this->getServer()->getOnlinePlayers() as $ps){
+  $ps->sendMessage(TextFormat::GREEN . .$world." [+] ".$name);
+     }
+  }
 }
+
+public function WorldQuitEvent( $e){
+$world = $e->getPlayer()->getLevel()->getName();
+$name = $e->getPlayer()->getName();
+if($this->getConfig()->get("WorldJoinAndQuit") == "true"){
+foreach($this->getServer()->getOnlinePlayers() as $ps){
+  $ps->sendMessage(TextFormat::RED . .$world." [-] ".$name);
+    }
+  }
 }
 
 public function PlayerFirstJoin(PlayerFirstJoinEvent $e){
 $firstjoinmessage = $this->getConfig()->get("firstjoinmessage");
 foreach($this->getServer()->getPlayers() as $ps){
-$ps->sendMessage($firstjoinmessage." ");
-}
-}
+  $ps->sendMessage($firstjoinmessage." ");
+    }
+  }
 }
 ?>
