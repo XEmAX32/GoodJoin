2d4c7f2e572f21e983496d9bdc752e0cb9abc0eb
XEmAX32 <XEmAX32@gmail.com>
2015-12-12 21:31:48 +0100

    Create Main2.php

---
+++ src/GoodJoin/Main2.php
@@ -0,0 +1,11 @@
+namespace GoodJoin
+
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\PlayerJoinEvent;
+use pocketmine\event\PlayerQuitEvent;
+
+class Main extends PluginBase implements Listener{
+
+}
